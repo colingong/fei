11c981341c11b8857b47622807124bfb5509e783
pf <colingong@hotmail.com>
2020-03-24 20:48:27 +0800

    modify setup.py: comment packages line

---
+++ docs/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='fei',
+      version='0.1',
+      description="My django demo",
+      long_description="",
+      author='Fei',
+      author_email='vxfs@qq.com',
+      license='TODO',
+    #   packages=['fei'],
+      zip_safe=False,
+      install_requires=[
+          'Django',
+          # 'Sphinx',
+          # ^^^ Not sure if this is needed on readthedocs.org
+          # 'something else?',
+          ],
+      )
