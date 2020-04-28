fc9a8aab647507f95ac98025ed6312d264f22bd1
pf <colingong@hotmail.com>
2020-04-28 10:27:23 +0800

    commit

--- fei/app_drf/urls.py
+++ fei/app_drf/urls.py
@@ -10,34 +10,11 @@ from . import views_v2
 
 app_name = 'app_drf'
 
-router = routers.DefaultRouter()
-router.register('users', views.UserViewSet)
-router.register('userextra', views.UserExtraViewSet)
-# router.register('feiviewset', views.FeiViewSet, basename='feiviewset')
-# router.register('user_and_extra', views_apiview_to_router.UserAndExtra, basename='custom_api')
-
 # for views_v2
 router_v2 = routers.DefaultRouter()
 router_v2.register('user', views_v2.UserViewSet)
 router_v2.register('product', views_v2.ProductViewSet)
 
 urlpatterns = [
-    # path('', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-    # # User and UserExtra combined view
-    # path('feiview/<int:pk>/', views.FeiView.as_view(), name='feiview'),
-    path('send_json/', views.send_json, name='send_json'),
-
-    # # views_drf1
-    # path('show_user/<int:pk>/', views_drf1.show_user, name='show_user'),
-    # path('full_info/<int:pk>/', views_drf1.full_user, name='full_user'),
-    # # path('user_and_extra/<int:pk>/', views_drf1.user_and_extra, name='user_and_extra'),
-
-    # # token demo
     # path('api-token-auth/', views_token.drf_views.obtain_auth_token, name='api-token-auth'),
-
-    # # views_customize_serializer
-    # path('user_all_info/<int:pk>/', views_customize_serializer.show_user_all_info, name='user-all-info'),
-
 ]
--- fei/app_drf/views_v2.py
+++ fei/app_drf/views_v2.py
@@ -7,15 +7,17 @@ from app_models.models import Category, Product, UserOrder, Warehouse, Supplier
 from rest_framework import serializers
 from rest_framework import viewsets
 
+# User serializer and ModelViewSet
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'first_name', 'last_name']
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
+# Product serializer and ModelViewSet
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -24,3 +26,4 @@ class ProductSerializer(serializers.ModelSerializer):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
