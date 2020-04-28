75dbcc1d48c236115d96a17f65774aa2ea0bacce
pf <colingong@hotmail.com>
2020-04-29 00:16:25 +0800

    v2 api

--- fei/app_drf/urls.py
+++ fei/app_drf/urls.py
@@ -14,6 +14,11 @@ app_name = 'app_drf'
 router_v2 = routers.DefaultRouter()
 router_v2.register('user', views_v2.UserViewSet)
 router_v2.register('product', views_v2.ProductViewSet)
+router_v2.register('category', views_v2.CategoryViewSet)
+router_v2.register('userorder', views_v2.UserOrderViewSet)
+router_v2.register('warehouse', views_v2.WarehouseViewSet)
+router_v2.register('supplier', views_v2.SupplierViewSet)
+router_v2.register('fulluser', views_v2.FullUserViewSet)
 
 urlpatterns = [
     # path('api-token-auth/', views_token.drf_views.obtain_auth_token, name='api-token-auth'),
--- fei/app_drf/views_v2.py
+++ fei/app_drf/views_v2.py
@@ -27,3 +27,97 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+# Category
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+# UserOrder
+class UserOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserOrder
+        fields = '__all__'
+
+class UserOrderViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = UserOrder.objects.all()
+    serializer_class = UserOrderSerializer
+
+class WarehouseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Warehouse
+        fields = '__all__'
+
+#    Warehouse, Supplier
+class WarehouseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
+
+# Supplier
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+
+class SupplierViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+# UserExtra  
+from django.core.exceptions import ObjectDoesNotExist
+class FullUserSerializer(serializers.ModelSerializer):
+    """显示用户的完整信息 User & UserExtra & UserAsset
+
+    Arguments:
+        weixin_openid: UserExtra / weixin_openid
+        phone: UserExtra / phone
+        qq: UserExtra / qq
+        balance: UserAsset / balance 
+    """
+    weixin_openid = serializers.SerializerMethodField('get_userextra_weixin_openid')
+    phone = serializers.SerializerMethodField('get_userextra_phone')
+    qq = serializers.SerializerMethodField('get_userextra_qq')
+    balance = serializers.SerializerMethodField('get_userasset_balance')
+
+    def get_userextra_weixin_openid(self, instance):
+        user = instance
+        print(f'---> type instance {type(user)}')
+        try:
+            return user.userextra.weixin_openid
+        except ObjectDoesNotExist as e:
+            return '-'
+
+    def get_userextra_phone(self, instance):
+        user = instance
+        try:
+            return user.userextra.phone
+        except ObjectDoesNotExist as e:
+            return '-'
+        
+    def get_userextra_qq(self, instance):
+        user = instance
+        try:
+            return user.userextra.qq
+        except ObjectDoesNotExist as e:
+            return '-'
+
+    def get_userasset_balance(self, instance):
+        user = instance
+        try:
+            return user.userasset.balance
+        except ObjectDoesNotExist as e:
+            return '-'
+
+    class Meta:
+        model = User
+        # fields = '__all__'
+        exclude = ['password', ]
+
+class FullUserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all().order_by('id')
+    serializer_class = FullUserSerializer
+#   UserExtra, UserAsset
