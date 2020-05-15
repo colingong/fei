618683ef3f8efbc286cf455bcccc3611bc52b5ae
pf <colingong@hotmail.com>
2020-05-15 23:18:39 +0800

    change style: site_root.html

--- fei/app_clientip/views.py
+++ fei/app_clientip/views.py
@@ -8,13 +8,17 @@ def root_echoip(request):
     """返回访问者的ip
 
         有时候想知道自已的公网ip，就直接用这个好了
+
+        先从nginx取'X-Real-Ip'）
         由于用了nginx作为反向代理，需要在nginx里如下配置，然后获取 X-Real-IP即可
             location / {
                 proxy_set_header Host $host;
-                proxy_set_header X-Real-IP  $remote_addr;
+                proxy_set_header X-Real-Ip  $remote_addr;
                 proxy_set_header X-Forwarded-For $remote_addr;
                 proxy_pass http://10.0.3.115;
             }
+
+        如果django前面没有nginx，就直接从 request.META里取 'REMOTE_ADDR'
     """
     ipaddress = request.headers.get('X-Real-Ip', request.META.get('REMOTE_ADDR'))
     d = {'Your IP': ipaddress}
