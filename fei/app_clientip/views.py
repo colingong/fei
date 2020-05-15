01ce84945ac2f61f94e7c24e54a120e35f36223c
pf <colingong@hotmail.com>
2020-05-15 22:35:20 +0800

    app_clientip: echo ip

--- fei/app_clientip/views.py
+++ fei/app_clientip/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
-
+from django.http import JsonResponse
 # Create your views here.
 def alive(request):
     return HttpResponse('alive - clientip')
 
 def root_echoip(request):
-    headers = request.headers
+    request_headers = request.headers
     print('ECHOIP --->')
-    print(headers)
-    return HttpResponse('ok')
+    print(request_headers)
+    echo_headers = ['X-Real-Ip', 'X-Forwarded-For']
+    d = {k: v for k, v in request.headers.items() if k in echo_headers}
+    return JsonResponse(d)
