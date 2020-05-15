0e222ed33012b6e403b4fadcd737666230037295
pf <colingong@hotmail.com>
2020-05-15 22:40:27 +0800

    app_clientip: change echo json

--- fei/app_clientip/views.py
+++ fei/app_clientip/views.py
@@ -5,9 +5,5 @@ def alive(request):
     return HttpResponse('alive - clientip')
 
 def root_echoip(request):
-    request_headers = request.headers
-    print('ECHOIP --->')
-    print(request_headers)
-    echo_headers = ['X-Real-Ip', 'X-Forwarded-For']
-    d = {k: v for k, v in request.headers.items() if k in echo_headers}
+    d = {'Your IP': request.headers.get('X-Real-Ip', 'Not get your ip! ')}
     return JsonResponse(d)
