fb4f3cac2091896d9ae92f0fce8107e02745a0c7
pf <colingong@hotmail.com>
2020-05-15 23:01:42 +0800

    app_clientip: get X-Real-IP from headers(nginx forworded), or get REMOTE_ADDR from request.META

--- fei/app_clientip/views.py
+++ fei/app_clientip/views.py
@@ -5,5 +5,17 @@ def alive(request):
     return HttpResponse('alive - clientip')
 
 def root_echoip(request):
-    d = {'Your IP': request.headers.get('X-Real-Ip', 'Not get your ip! ')}
+    """返回访问者的ip
+
+        有时候想知道自已的公网ip，就直接用这个好了
+        由于用了nginx作为反向代理，需要在nginx里如下配置，然后获取 X-Real-IP即可
+            location / {
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP  $remote_addr;
+                proxy_set_header X-Forwarded-For $remote_addr;
+                proxy_pass http://10.0.3.115;
+            }
+    """
+    ipaddress = request.headers.get('X-Real-Ip', request.META.get('REMOTE_ADDR'))
+    d = {'Your IP': ipaddress}
     return JsonResponse(d)
