1aea1a5f7bda2ab6dc30ca91794c6abd1d3239d8
pf <colingong@hotmail.com>
2020-05-20 01:39:20 +0800

    create script for generate test product

---
+++ fei/dumpdata/generate_data.py
@@ -0,0 +1,61 @@
+"""生成测试数据
+"""
+import sys, os
+
+from random import randint
+PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname((os.path.abspath(__file__))))
+sys.path.append(PROJECT_ROOT_DIR)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main_settings.settings")
+import django
+django.setup()
+
+# setup_environ(settings)
+import random
+from app_models.models import Category, Warehouse, Product
+
+class ProductData(object):
+    """生成测试的product
+
+    一个product记录是这个样子的：
+            "category": 2,
+            "warehouse": 7,
+            "product_name": "Gummy Bear VC",
+            "product_description": "小熊糖维生素C，150粒",
+            "product_price": "150.00",
+            "product_stock": 100
+    
+    测试数据生成过程
+        随机选一个category
+        随机选一个warehouse
+        生成一个product_name
+        生成一个product_description
+        生成一个product_price
+        生成一个product_stock
+    """
+    def __init__(self):
+        # self.category = Category.objects.values("id").order_by("?")[0]
+        self._rand_str = str(random.randint(1, 999999))
+
+        self.p = Product()
+        self.p.category = Category.objects.all().order_by("?")[0]
+        self.p.warehouse = Warehouse.objects.all().order_by("?")[0]
+        self.p.product_name = "测试产品_" + self._rand_str
+        self.p.product_description = "产品描述_" + self._rand_str
+        self.p.product_price = random.uniform(0.01, 100000.00)
+        self.p.product_stock = random.randint(0, 9999)
+
+    def prod_name(self):
+        return "测试产品_" + str(random.randint(1, 9999999))
+
+    def prod_description(self):
+        return "产品描述是随机的_" + str(random.randint(1, 9999999))
+
+# p = ProductData()
+# print(p.category)
+if __name__ == '__main__':
+    product = ProductData()
+    print(product.p.__dict__)
+    for i in range(30):
+        product = ProductData()
+        product.p.save()
+        print(f'处理记录数量： {i}')
