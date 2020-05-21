8a28bc2c3c8c82f7321b56e236bf90db26764bed
pf <colingong@hotmail.com>
2020-05-21 20:28:31 +0800

    change to uwsgi

---
+++ fei/start_dev_uwsgi.sh
@@ -0,0 +1,1 @@
+uwsgi dev_mac.ini --static-map /static=/Users/pengfei/Documents/wns/github_repos/fei_home/fei/collect_serve
---
+++ fei/start_prod_uwsgi.sh
@@ -0,0 +1,1 @@
+uwsgi prod_docker.ini --static-map /static=/usr/src/app/collect_serve
