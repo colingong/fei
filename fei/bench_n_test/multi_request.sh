2c427fbe5627c9ca99b47073f046c7c4569eb618
pf <colingong@hotmail.com>
2020-05-21 23:00:13 +0800

    change bench script

--- fei/bench_n_test/multi_request.sh
+++ fei/bench_n_test/multi_request.sh
@@ -5,7 +5,7 @@ CACHE_OR_NOCACHE=$3
 
 req() {
     for (( i=${START}; i<${END}; i++ )); do
-        curl demo.hhxx.me/cache/${CACHE_OR_NOCACHE}/$i/
+        curl 127.0.0.1:8000/cache/${CACHE_OR_NOCACHE}/$i/
         echo 
         # curl demo.hhxx.me/cache/cached/${COUNT}/
     done
