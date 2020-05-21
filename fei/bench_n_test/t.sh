4372eb5cba0df4db4fcb69d3083a4d7ec3a84441
pf <colingong@hotmail.com>
2020-05-22 00:56:27 +0800

    add t.sh

---
+++ fei/bench_n_test/t.sh
@@ -0,0 +1,4 @@
+python mock_request.py 1 5000 > 1.local_log &
+python mock_request.py 5001 10000 > 2.local_log &
+python mock_request.py 10001 15000 > 3.local_log &
+python mock_request.py 15000 20000 > 4.local_log &
