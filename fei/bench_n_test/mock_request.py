476c802002e5cbaf9839bd32daa0f56ca8a63b8e
pf <colingong@hotmail.com>
2020-05-21 23:32:10 +0800

    update mock_request.py

--- fei/bench_n_test/mock_request.py
+++ fei/bench_n_test/mock_request.py
@@ -9,9 +9,9 @@ def func(url):
 if __name__ == '__main__':
     range_start = int(sys.argv[1])
     range_end = int(sys.argv[2])
-    start = time.time()
+    start = int(time.time())
     for id in range(range_start, range_end):
         url = f"http://127.0.0.1:8000/cache/cached/{id}/"
         func(url)
-    end = time.time()
+    end = int(time.time())
     print(f'--- used time : {end - start} s ---\n')
