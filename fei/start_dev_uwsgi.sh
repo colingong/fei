9628550d0a7026a9b46fc84331414acddfd479bc
pf <colingong@hotmail.com>
2020-05-26 14:53:08 +0800

    .

--- fei/start_dev_uwsgi.sh
+++ fei/start_dev_uwsgi.sh
@@ -1,1 +1,2 @@
+# set -e source setup_env.sh
 uwsgi dev_mac.ini --static-map /static=/Users/pengfei/Documents/wns/github_repos/fei_home/fei/collect_serve
--- fei/start_prod_uwsgi.sh
+++ fei/start_prod_uwsgi.sh
@@ -1,1 +1,2 @@
+set -e source /root/setup_env.sh
 uwsgi prod_docker.ini --static-map /static=/usr/src/app/collect_serve
